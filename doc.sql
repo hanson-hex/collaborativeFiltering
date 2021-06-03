831bbe719414541373d8bcca5801fb8370f24a95
RemyHe <hex192039@163.com>
2021-06-03 20:05:31 +0800

    修复了bug

--- doc.sql
+++ doc.sql
@@ -2,3 +2,4 @@
   update [dbo].[t_AutoCoding_Entity] set isRoot = 0 where UniqueName = 'services'
 
   update [dbo].[t_AutoCoding_FieldDocExtension] set Description = 'Specific email addresses that offline message are sent to. Available and required when Offline Message Mail Type is "toEmailAddresses".' where FieldId = 'C5B25329-7C46-EB11-8100-00155D081D0B'
+
